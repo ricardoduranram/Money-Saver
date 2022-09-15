c4517814c6f8fe76ee253d9946a6c0fe19c5a223
Ricardo.Duran <ricardo.duran.ram@gmail.com>
2022-09-14 21:09:25 -0700

    auto mapper implementation

--- Source/Source/Horeb.MoneySaver.Persistency/Repository.cs
+++ Source/Source/Horeb.MoneySaver.Persistency/Repository.cs
@@ -1,11 +1,5 @@
 ﻿using AutoMapper;
 using Horeb.Infrastructure.Data;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Horeb.MoneySaver.Persistency
 {
---
+++ Source/Source/Horeb.MoneySaver.Persistency/Services/MappingServices/RepositoryMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Horeb.Domain.TransactionModule;
+using Horeb.Domain.WalletModule;
+using Horeb.MoneySaver.Persistency.EntityDataModels;
+
+namespace Horeb.MoneySaver.API.Services.MappingServices
+{
+    public class RepositoryMappingProfile: Profile
+    {
+        public RepositoryMappingProfile()
+        {
+            CreateMap<Wallet, WalletModel>();
+            CreateMap<Transaction, TransactionModel>();
+            CreateMap<TransactionCategory, TransactionCategoryModel>();
+        }
+    }
+}
